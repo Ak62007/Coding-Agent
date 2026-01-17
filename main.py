854772bb30d0902efada25a9e8efcf34bea6137a
Ak62007 <ied10007.23@bitmesra.ac.in>
2026-01-17 02:48:28 +0000

    initial already started working on the agent

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import argparse
+from typing import Any
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
+parser = argparse.ArgumentParser(description="Coding bot")
+parser.add_argument("user_prompt", type=str, help="Tell me whatever you wanna ask..")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+load_dotenv()
+try:
+    api_key = os.environ.get("GEMINI_API_KEY")
+except:
+    raise RuntimeError()
+
+client = genai.Client(api_key=api_key)
+
+def llm_call(args, contents: list[Any], model_name: str = "gemini-2.5-flash") -> str:
+    """
+    Docstring for llm_call
+    
+    :param prompt: Prompt to the llm
+    :type prompt: str
+    :return: response from the llm
+    :rtype: str
+    """
+    response =  client.models.generate_content(
+                model=model_name,
+                contents=contents
+                )
+    
+    if response.usage_metadata:
+        token_sent = response.usage_metadata.prompt_token_count
+        token_received = response.usage_metadata.candidates_token_count
+    else:
+        raise RuntimeError("Api Call wasn't successfull.")
+    
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {token_sent}")
+        print(f"Response tokens: {token_received}")
+        print(f"Response:\n{response.text}")
+    else:
+        print(f"Response:\n{response.text}")
+    
+    return response.text
+    
+    
+    
+def main():
+    # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = llm_call(args=args, contents=messages)
+
+
+if __name__ == "__main__":
+    main()
